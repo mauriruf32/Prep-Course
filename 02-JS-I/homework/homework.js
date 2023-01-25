3b448cb20845180191a0a27f22a8751541bef64f
unknown <mauriruffini32@gmail.com>
2023-01-25 18:48:58 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -87,7 +87,7 @@ function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num > 50) {
+  if (num < 50) {
   return true;
   }
   return false;
